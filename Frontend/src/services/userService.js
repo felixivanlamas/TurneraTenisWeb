0d622d90602c3cf8f31abcbb7ed9d6ab516e6f85
Facundo Neiman <71708904+FacuNeiman03@users.noreply.github.com>
2023-06-15 22:21:05 -0300

    reservas view

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -2,20 +2,41 @@ import axiosClient from "./axiosClient.js";
 const apiClient = axiosClient.getApiClient();
 
 export const userService = {
-    login(usuario) {
-        //Utilizo api Client que es el objeto inicializado de axios
-        return apiClient.post('/usuarios/login', usuario);
-       // axios pero instanciado con la direccion y las caracteristicas de la peticion.
-    },
-    register(usuario){
-        return apiClient.post('/usuarios/register', usuario);
-    },
+  login(usuario) {
+    //Utilizo api Client que es el objeto inicializado de axios
+    return apiClient.post("/usuarios/login", usuario);
+    // axios pero instanciado con la direccion y las caracteristicas de la peticion.
+  },
+  register(usuario) {
+    return apiClient.post("/usuarios/register", usuario);
+  },
 
-    getUser(id){
-        return apiClient.get(`/usuarios/${id}`);
-    },
-    
-    reservar(id,reserva){
-        return apiClient.post('/usuarios/:id',reserva);
-    }
-}
+  getUser(/*id*/) {
+    //return apiClient.get(`/usuarios/${id}`);
+    return {
+      username: "boris",
+      email: "b@b.com",
+      contrasenia: "123",
+      reservas: [
+        {
+          titulo: "Cancha n°1",
+          dia: ["Jueves"],
+          horario: "23:00",
+        },
+        {
+          titulo: "Cancha n°3",
+          dia: ["Sabado"],
+          horario: "12:00",
+        },
+      ],
+    };
+  },
+
+  reservar(id, reserva) {
+    return apiClient.post("/usuarios/:id", reserva);
+  },
+
+  eliminarReserva(reserva) {
+    //
+  },
+};
