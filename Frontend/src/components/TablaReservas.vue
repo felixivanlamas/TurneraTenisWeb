a9f184824b4e2229f92c223a774f1b00911d59c1
Felix Ivan Lamas <felix.lamas@hotmail.com>
2023-07-04 21:55:15 -0300

    update

--- Frontend/src/components/TablaReservas.vue
+++ Frontend/src/components/TablaReservas.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-    <h3>Reservas de {{ canchaSeleccionada.titulo }}</h3>
+  <div class="col-6">
+    <h3>Reservas</h3>
     <table class="table">
       <thead>
         <tr>
           <th>Día</th>
-          <th>Usuarios</th>
+          <th>Horario</th>
+          <th>Usuario</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="reservaDia in reservasPorDia" :key="reservaDia.dia">
-          <td>{{ reservaDia.dia }}</td>
-          <td>
-            <ul>
-              <li v-for="reserva in reservaDia.reservas" :key="reserva.username">
-                {{ reserva.username }} - {{ reserva.horario }}
-              </li>
-            </ul>
-          </td>
-        </tr>
+        <template v-for="reservaDia in reservasPorDia">
+          <tr v-for="reserva in reservaDia.reservas" :key="reserva.id">
+            <td v-if="reservaDia.reservas.indexOf(reserva) === 0" :rowspan="reservaDia.reservas.length">
+              {{ reservaDia.dia }}
+            </td>
+            <td>{{ reserva.horario }}</td>
+            <td>{{ reserva.username }}</td>
+          </tr>
+        </template>
       </tbody>
     </table>
   </div>
